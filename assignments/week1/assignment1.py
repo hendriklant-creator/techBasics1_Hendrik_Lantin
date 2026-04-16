707ec6b97717e638e558e4a09da5fcdfd9903b6a
Hendrik Lantin <hendriklant@gmail.com>
2026-04-16 10:15:16 +0200

    Update assignment1.py

--- assignments/week1/assignment1.py
+++ assignments/week1/assignment1.py
@@ -26,3 +26,62 @@ ________________________________________________ ________
 
 
 
+import time
+import os
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+# Frame 1 - Character on left
+frame1 = """
+    .-""-.
+   /-.{}  \\         
+   | _\__.|      OOOOO
+   \\/^)^ \\/       OOO
+    \\ =  /       OOOOO
+    I    I         I
+    I    I---------I
+    I    I---------I
+    I    I         I
+    I    I         I
+    I    I         I
+"""
+
+# Frame 2 - Character moves right
+frame2 = """
+        .-""-.
+       /-.{}  \\         
+       | _\__.|      OOOOO
+       \\/^)^ \\/       OOO
+        \\ =  /       OOOOO
+        I    I         I
+        I    I---------I
+        I    I---------I
+        I    I         I
+        I    I         I
+        I    I         I
+"""
+
+# Frame 3 - Character moves more right
+frame3 = """
+            .-""-.
+           /-.{}  \\         
+           | _\__.|      OOOOO
+           \\/^)^ \\/       OOO
+            \\ =  /       OOOOO
+            I    I         I
+            I    I---------I
+            I    I---------I
+            I    I         I
+            I    I         I
+            I    I         I
+"""
+
+frames = [frame1, frame2, frame3, frame2, frame1]
+
+# Play animation
+for i in range(5):  # Loop 5 times
+    clear_screen()
+    print(frames[i % len(frames)])
+    time.sleep(0.5)  # Wait 0.5 seconds between frames
+
