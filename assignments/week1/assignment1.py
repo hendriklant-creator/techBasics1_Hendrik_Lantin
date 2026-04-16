dfab54153bb4c6802f7c71d832633203cd7c8df9
Hendrik Lantin <hendriklant@gmail.com>
2026-04-16 10:12:52 +0200

    Update assignment1.py

--- assignments/week1/assignment1.py
+++ assignments/week1/assignment1.py
@@ -8,5 +8,18 @@
        I    I---------I
        I    I         I
        I    I         I
-       I    I         I
-________________________________________________________
+       I    I         I           
+________________________________________________ ________
+
+       O              OOOOO
+      /|\             OOO
+       / \           OOOOO
+
+
+# Frame 1
+print("Frame 1:")
+# ASCII art
+
+# Frame 2
+print("Frame 2:")
+# ASCII art (slightly modified)
